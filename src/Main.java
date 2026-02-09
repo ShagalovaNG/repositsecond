fb629d04adf8581cadb1a36c4f6ced8e1743e46f
Natalya Shagalova <nataliyshagalyalya@gmail.com>
2026-02-10 01:28:04 +0700

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int initialAccount=100; //начальный счет
-        int summa =1100; //сумма пополнения
+        int initialAccount = 100; //начальный счет
+        int summa = 1100; //сумма пополнения
         int bonus;
-        if ((initialAccount+summa)>1000) {
+        if ((initialAccount + summa) > 1000) {
             bonus = summa / 100;
-        } else { bonus=0;
-            }
-        System.out.println("Бонус = "+bonus);
-        System.out.println("Сумма на счете = "+ (initialAccount+summa+bonus));
+        } else {
+            bonus = 0;
         }
+        System.out.println("Бонус = " + bonus);
+        System.out.println("Сумма на счете = " + (initialAccount + summa + bonus));
     }
+}
